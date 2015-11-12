f1a2344d6da87b01e0b1ba611d948c05286a7399
donnaaboise <donna.calhoun@gmail.com>
2015-11-12 14:08:27 -0700

    Initial commit of Teton Dam project

---
+++ examples/flooding/tetondam/setplot.py
@@ -0,0 +1,170 @@
+
+"""
+Set up the plot figures, axes, and items to be done for each frame.
+
+This module is imported by the plotting routines and then the
+function setplot is called to set the plot parameters.
+
+"""
+
+import numpy
+a = 1.
+sigma = 0.5
+h0 = 115.0
+grav = 9.81
+
+#--------------------------
+def setplot(plotdata):
+#--------------------------
+
+    """
+    Specify what is to be plotted at each frame.
+    Input:  plotdata, an instance of pyclaw.plotters.data.ClawPlotData.
+    Output: a modified version of plotdata.
+
+    """
+
+
+    from clawpack.visclaw import colormaps, geoplot
+
+    plotdata.clearfigures()  # clear any old figures,axes,items data
+
+    def set_drytol(current_data):
+        # The drytol parameter is used in masking land and water and
+        # affects what color map is used for cells with small water depth h.
+        # The cell will be plotted as dry if h < drytol.
+        # The best value to use often depends on the application and can
+        # be set here (measured in meters):
+        current_data.user["drytol"] = 1.e-3
+
+    plotdata.beforeframe = set_drytol
+
+    #-----------------------------------------
+    # Figure for pcolor plot
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='pcolor', figno=0)
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes('pcolor')
+    plotaxes.title = 'Surface'
+    plotaxes.scaled = True
+
+
+    dark_blue = [0.2,0.2,0.7];
+    light_blue = [0.7,0.7,1.0];
+    flooding_colormap = colormaps.make_colormap({ -1.0:light_blue,
+                                                 1.0:dark_blue})
+    # Water
+    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
+    plotitem.plot_var = geoplot.surface
+    # plotitem.pcolor_cmap = geoplot.tsunami_colormap
+    plotitem.pcolor_cmap = flooding_colormap
+    plotitem.pcolor_cmin = 0
+    plotitem.pcolor_cmax = 115
+    plotitem.add_colorbar = True
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+
+    # Land
+    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
+    plotitem.plot_var = geoplot.land
+    plotitem.pcolor_cmap = geoplot.land_colors
+    plotitem.pcolor_cmin = 0.0
+    plotitem.pcolor_cmax = 300
+    plotitem.add_colorbar = False
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 0
+    plotaxes.xlimits = [0,24990]
+    plotaxes.ylimits = [0,5370]
+
+    # Add contour lines of bathymetry:
+    plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
+    plotitem.plot_var = geoplot.topo
+    from numpy import arange, linspace
+    plotitem.contour_levels = linspace(0,300,50)
+    plotitem.amr_contour_colors = ['k']  # color on each level
+    plotitem.kwargs = {'linestyles':'solid'}
+    plotitem.amr_contour_show = [1]
+    plotitem.celledges_show = 0
+    plotitem.patchedges_show = 0
+    plotitem.show = False
+
+
+    def addgauges(current_data):
+        from clawpack.visclaw import gaugetools
+        gaugetools.plot_gauge_locations(current_data.plotdata, \
+                gaugenos='all', format_string='ko', add_labels=True)
+
+    plotaxes.afteraxes = addgauges
+
+
+    #-----------------------------------------
+    # Figures for gauges
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='Surface at gauges', figno=300, \
+                    type='each_gauge')
+    plotfigure.clf_each_gauge = True
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes()
+    plotaxes.xlimits = 'auto'
+    plotaxes.ylimits = 'auto'
+    plotaxes.title = 'Surface'
+
+    # Plot surface as blue curve:
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    plotitem.plot_var = 3
+    plotitem.plotstyle = 'b-'
+
+    # Plot topo as green curve:
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    plotitem.show = False
+
+    def gaugetopo(current_data):
+        q = current_data.q
+        h = q[0,:]
+        eta = q[3,:]
+        topo = eta - h
+        return topo
+
+    plotitem.plot_var = gaugetopo
+    plotitem.plotstyle = 'g-'
+
+    def add_zeroline(current_data):
+        from pylab import plot, legend, xticks, floor, axis, xlabel
+        t = current_data.t
+        gaugeno = current_data.gaugeno
+
+        if gaugeno == 32412:
+            try:
+                plot(TG32412[:,0], TG32412[:,1], 'r')
+                legend(['GeoClaw','Obs'],loc='lower right')
+            except: pass
+            axis((0,t.max(),-0.3,0.3))
+
+        plot(t, 0*t, 'k')
+        n = int(floor(t.max()/3600.) + 2)
+        xticks([3600*i for i in range(n)], ['%i' % i for i in range(n)])
+        xlabel('time (hours)')
+
+    plotaxes.afteraxes = add_zeroline
+
+
+    #-----------------------------------------
+
+    # Parameters used only when creating html and/or latex hardcopy
+    # e.g., via pyclaw.plotters.frametools.printframes:
+
+    plotdata.printfigs = True                # print figures
+    plotdata.print_format = 'png'            # file format
+    plotdata.print_framenos = 'all'          # list of frames to print
+    plotdata.print_gaugenos = []             # list of gauges to print
+    plotdata.print_fignos = 'all'            # list of figures to print
+    plotdata.html = True                     # create html files of plots?
+    plotdata.html_homelink = '../README.html'   # pointer for top of index
+    plotdata.latex = False                    # create latex file of plots?
+    plotdata.latex_figsperline = 2           # layout of plots
+    plotdata.latex_framesperline = 1         # layout of plots
+    plotdata.latex_makepdf = False           # also run pdflatex?
+
+    return plotdata
