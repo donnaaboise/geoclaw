53da011e2096714665941c66ab5cba8380f6976f
donnaaboise <donnacalhoun@boisestate.edu>
2019-08-05 08:54:43 +0100

    (tetondam) Remove large topo files

--- examples/flooding/tetondam/fix_topo.m
+++ examples/flooding/tetondam/fix_topo.m
@@ -1,5 +1,11 @@
 function fix_topo(fname,mx,my,fname_out)
 
+% ncols        1798
+% nrows        1042
+% xllcorner    -121.795724838159
+% yllcorner    39.364837972813
+% cellsize     0.000331742455
+
 d = load(fname);
 
 d1 = reshape(d,my,mx)';
