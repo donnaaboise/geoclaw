dbba6c3f2aee4875eb4359d403f436d6b7363938
donnaaboise <donnacalhoun@boisestate.edu>
2020-07-16 12:39:41 -0600

    (tetondam) Add more matlab files

--- examples/flooding/tetondam/add_regions.m
+++ examples/flooding/tetondam/add_regions.m
@@ -1,4 +1,10 @@
-function rhout = add_regions(t)
+function rhout = add_regions(t,format)
+
+if (nargin < 2)
+    format = 'ForestClaw';    
+end
+
+use_forestclaw = strcmpi(format,'forestclaw');
 
 o = findobj(gcf,'Tag','region');
 if (~isempty(o))
@@ -6,25 +12,35 @@ if (~isempty(o))
 end
 
 np = get(gca,'NextPlot');
+set(gca,'NextPlot','add');
 
-if (~exist('regions.data'))
+if (~exist('regions.data','file'))
     fprintf('File regions.data does not exist.  No regions will be plotted.\n');
     return
 end
 
 fid = fopen('regions.data','r');
-for i = 1:5,
+for i = 1:5
     % Read first five lines of comments
-    l = fgetl(fid);
+    fgetl(fid);
 end
-l = fgetl(fid);  % blank line
+fgetl(fid);  % blank line
 l = fgetl(fid);  % Get number of gauges
 num_regions = sscanf(l,'%d',1);
 
-c = {'r','b','y','c','w','g','k'};   % Colors for each region
+c = {'w','w','w','w','w','w','w'};   % Colors for each region
+
+if (use_forestclaw)
+    zmax = 0;
+    zl = [-20,0];
+else
+    % Use AMRClaw : set maxlevel = 20
+    zmax = 20;
+    zl = [0,20];
+end
 
 region_handles = zeros(num_regions,1);
-for n = 1:num_regions,
+for n = 1:num_regions
     l = fgetl(fid);
     data = sscanf(l,'%d %d %e %e %e %e %e %e',Inf);
     minlevel = data(1);
@@ -37,12 +53,17 @@ for n = 1:num_regions,
     y1 = data(8);
     xp = [x0 x1 x1 x0 x0];
     yp = [y0 y0 y1 y1 y0];
+    zp = 0*xp + zmax;
     if (t0 <= t && t <= t1)
-        hg = plot(xp,yp,'linewidth',2,'color',c{mod(n-1,7)+1});
+        hg = plot3(xp,yp,zp,'linewidth',2,'color',c{mod(n-1,7)+1});
         set(hg,'Tag','region');
     end
     zl = zlim;
-    set(gca,'zlim',[min(zl),0]);
+    if (use_forestclaw)
+        set(gca,'zlim',[min(zl),0]);
+    else
+        set(gca,'zlim',[0,max(zl)]);
+    end
     hold on;
 end
 
